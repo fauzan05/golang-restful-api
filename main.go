763f017a344e2a0011d99e84da43455c33d27a6f
Fauzan Nur Hidayat <fauzannurhidayat8@gmail.com>
2024-03-17 22:21:46 +0700

    memisahkan logic routing

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -23,13 +22,7 @@ func main() {
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
-	router := httprouter.New()
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+	router := app.NewRouter(categoryController)
 	
 	// ketika mengakses route dan ada error maka akan dieksekusi
 	router.PanicHandler = exception.ErrorHandler
